3163f463fc2862ee7f3e4f266426b3d55ef714e3
Kristina Cerniauskaite <k.cerniauskaite@gmail.com>
2022-09-13 21:10:16 +0200

    Add logic to create some default users and dialogs and safe them to the database when the app starts

--- src/main/java/com/kristina/dataapi/DataApiApplication.java
+++ src/main/java/com/kristina/dataapi/DataApiApplication.java
@@ -1,12 +1,28 @@
 package com.kristina.dataapi;
 
+import com.kristina.dataapi.customer.CustomerService;
+import com.kristina.dataapi.customer.model.Customer;
+import com.kristina.dataapi.dialog.DialogService;
+import com.kristina.dataapi.dialog.model.Dialog;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class DataApiApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(DataApiApplication.class, args);
+    }
+    @Bean
+    CommandLineRunner initDB(DialogService dialogService, CustomerService customerService){
+        return  args -> {
+            dialogService.saveDialog(new Dialog(new Customer()));
+            dialogService.saveDialog(new Dialog(new Customer()));
+            dialogService.saveDialog(new Dialog(new Customer()));
+            dialogService.saveDialog(new Dialog(new Customer()));
+
+        };
     }
 }
